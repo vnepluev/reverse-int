cde9ce358e3329a48f0d357180ecb08c300adeb8
Valeriy V Nepluev <greenet2007@gmail.com>
2021-03-14 13:47:32 +0300

    start: reverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = '' + n;
+    
+    if (n < 0) {
+        result = Math.abs(n) + '';
+    }
+    
+    result = result.split('').reverse().join('');
+
+    return result;
 }
